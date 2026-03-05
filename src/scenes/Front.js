51a942b04aa105bec2d80f8eaf6f4ca1f10d91cd
JaredMel <jareddmelendez1764@gmail.com>
2026-03-05 12:45:12 -0800

    Added a reset for when guess is wrong

--- src/scenes/Front.js
+++ src/scenes/Front.js
@@ -13,12 +13,16 @@ class Front extends Phaser.Scene {
         let head = this.add.image(game.config.width/2, game.config.height/2, 'temp').setOrigin(0.5,0.5).setInteractive()
 
         head.on('pointerdown', function () {
-            currentCombo[index] = 1
-            console.log(currentCombo[index])
+            currentCombo[index] = 0
+            console.log(currentCombo[index]) // DEBUG
             if (currentCombo[index] == correctCombo[index]) {
                 if (currentCombo.length == correctCombo.length) {
                     switchScenes = true
                 }
+            } else {
+                console.log("reset") // DEBUG
+                currentCombo = []
+                index = 0
             }
         })
     }
